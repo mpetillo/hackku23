1de80e5c16b99ed327632c776f3be59dbc3de4bf
Matthew <mpetillo@ku.edu>
2023-04-15 03:22:04 -0500

    "3:22 AM Commit"

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for
+import json
 app = Flask(__name__)
 
 @app.route('/')
@@ -12,6 +13,19 @@ def recipes():
 @app.route('/info')
 def info():
     return render_template('info.html')
+
+@app.route('/API/GetDropDownConfigurations/', methods=['GET'])
+def configureDropDowns():
+    cuisine, diets, ingredients, intolerences = {}, {}, {}, {}
+    with open("cuisine.json", "r") as f:
+        cuisine = json.load(f)
+    with open("diets.json", "r") as f:
+        diets = json.load(f)
+    with open("ingredients.json", "r") as f:
+        ingredients = json.load(f)
+    with open("intolerences.json", "r") as f:
+        intolerences = json.load(f)
+    return [cuisine, diets, ingredients, intolerences]
 
 if __name__ == '__main__':
     app.run()
---
+++ static/functions.js
@@ -0,0 +1,31 @@
+function createCuisine(cuisineJSON){
+    const cuisine = JSON.parse(cuisineJSON);
+    document.write("<h3>Which Cuisine Types are you in the mood for today?</h3>");
+    for (i = 0; i < cuisine["cuisinetypes"].length; i++){
+        document.write('<input type="checkbox" id="'+cuisine["cuisinetypes"][i]+'" value="'+cuisine["cuisinetypes"][i]+'"');
+        document.write("<label>"+cuisine["cuisinetypes"][i]+"</label>");
+    }
+}
+
+function createDiets(dietsJSON){
+    const cuisine = JSON.parse(cuisineJSON);
+    document.write("<h3>Which Cuisine Types are you in the mood for today?</h3>");
+    for (i = 0; i < cuisine["cuisinetypes"].length; i++){
+        document.write('<input type="checkbox" id="'+cuisine["cuisinetypes"][i]+'" value="'+cuisine["cuisinetypes"][i]+'"');
+        document.write("<label>"+cuisine["cuisinetypes"][i]+"</label>");
+    }
+}
+
+function createIngredients(ingredientsJSON){
+
+}
+
+function createIntolerences(intolerencesJSON){
+
+}
+
+function callJSONFiles(){
+    console.log("POP!");
+    let apilist = fetch('/API/GetDropDownConfigurations', {Method:'GET'});
+    createCuisine(apilist[0]);
+}
