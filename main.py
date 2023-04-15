ec952d058608009d654d70969d8af022c78469c4
ethanwheat <ethanwheat@ku.edu>
2023-04-14 22:15:31 -0500

    Updated Website
    Some site links not functional yet

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
+
+@app.route('/recipes')
+def recipes():
+    return render_template('recipe.html')
+
+@app.route('/info')
+def info():
+    return render_template('info.html')
 
 if __name__ == '__main__':
     app.run()
